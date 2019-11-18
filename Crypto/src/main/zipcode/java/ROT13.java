2167857c9f1db0d988d9fafc7d46709eac86fd4f
MannyMonocle <eetumadu@gmail.com>
2019-11-17 23:35:21 -0500

    submitting

--- Crypto/src/main/zipcode/java/ROT13.java
+++ Crypto/src/main/zipcode/java/ROT13.java
@@ -1,4 +1,6 @@
-public class ROT13  {
+public class ROT13 {
+    String message, encryptedMessage = "";
+
 
     ROT13(Character cs, Character cf) {
     }
@@ -21,8 +23,37 @@ public class ROT13  {
     }
 
     public static String rotate(String s, Character c) {
+        int key;
+        char ch;
+        String end = "";
+        if (Character.isUpperCase(c)) {
+            key = c - 'A';
+        } else {
+            key = c - 'a';
+        }
 
-        return "";
+        for (int i = 0; i < s.length(); i++) {
+            ch = s.charAt(i);
+
+            if ((int) ch >= (int) 'a' && (int) ch <= (int) 'z') {
+                ch = (char) (ch + key);
+
+
+                if ((int) ch > (int) 'z') {
+                    ch = (char) (ch - 'z' + 'a' - 1);
+                }
+                end += (char) ch;
+
+            } else if ((int) ch >= (int) 'A' && (int) ch <= (int) 'Z') {
+                ch = (char) (ch - 'Z' + 'A' - 1);
+                if ((int) ch > (int) 'Z') {
+                    ch = (char) (ch - 'Z' + 'A' - 1);
+                }
+                end += (char) ch;
+            }
+        }
+
+        return end;
     }
 
 }
